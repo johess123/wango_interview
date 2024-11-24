31810fd8a6a32e9bad1bc879487684070fbe7454
johess123 <s109213067@mail1.ncnu.edu.tw>
2024-11-24 21:14:30 +0800

    add_setting_env

--- dbConfig.py
+++ dbConfig.py
@@ -1,12 +1,21 @@
 import mysql.connector
 
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+# 載入 .env
+BASE_DIR = Path(__file__)
+ENV_PATH = BASE_DIR / "setting" / ".env"
+load_dotenv(dotenv_path=ENV_PATH)
+
 try:
     conn = mysql.connector.connect(
-        user="kenny",
-        password="Kenny061256",
-        host="127.0.0.1",
-        port=3306,
-        database="recognition"
+        user = os.getenv("MYSQL_USER")
+        password = os.getenv("MYSQL_PASSWORD")
+        host = os.getenv("MYSQL_HOST")
+        port = int(os.getenv("MYSQL_PORT"))
+        database = os.getenv("MYSQL_DATABASE")
     )
 except:
     print("Error connecting to DB")
